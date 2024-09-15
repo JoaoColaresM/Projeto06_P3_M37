af4205766621786f9fae7c605c870fbfbab2a264
João Colares <joaocolaresm@gmail.com>
2024-09-14 21:14:51 -0300

    aula_2

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -1,45 +1,31 @@
 import ProductsList from "../../components/ProductsList"
-import { Game } from "../../pages/Home"
-import { useEffect, useState } from "react"
+import {
+  useGetActionGamesQuery,
+  useGetSportGamesQuery,
+  useGetSimulationGamesQuery,
+  useGetFightGamesQuery,
+  useGetRpgGamesQuery
+} from '../../services/api'
 
 const Categories = () => {
-  const [gameAcao, setGameAcao] = useState<Game[]>([])
-  const [gameEsportes, setGameEsportes] = useState<Game[]>([])
-  const [gameSimulacao, setGameSimulacao] = useState<Game[]>([])
-  const [gameLuta, setGameLuta] = useState<Game[]>([])
-  const [gameRpg, setGameRpg] = useState<Game[]>([])
+  const { data: actionGames } = useGetActionGamesQuery()
+  const { data: sportnGames } = useGetSportGamesQuery()
+  const { data: simulationGames } = useGetSimulationGamesQuery()
+  const { data: fightGames } = useGetFightGamesQuery()
+  const { data: rpgGames } = useGetRpgGamesQuery()
 
-  useEffect(() => {
-    fetch('https://fake-api-tau.vercel.app/api/eplay/acao')
-      .then((res) => res.json())
-      .then((res) => setGameAcao(res))
-
-    fetch('https://fake-api-tau.vercel.app/api/eplay/esportes')
-      .then((res) => res.json())
-      .then((res) => setGameEsportes(res))
-
-    fetch('https://fake-api-tau.vercel.app/api/eplay/simulacao')
-      .then((res) => res.json())
-      .then((res) => setGameSimulacao(res))
-
-    fetch('https://fake-api-tau.vercel.app/api/eplay/luta')
-      .then((res) => res.json())
-      .then((res) => setGameLuta(res))
-
-    fetch('https://fake-api-tau.vercel.app/api/eplay/rpg')
-      .then((res) => res.json())
-      .then((res) => setGameRpg(res))
-  }, [])
-
-  return (
-    <>
-      <ProductsList games={gameAcao} title='Ação' background='gray' />
-      <ProductsList games={gameEsportes} title='Esportes' background='black' />
-      <ProductsList games={gameLuta} title='Luta' background='gray' />
-      <ProductsList games={gameRpg} title='rpg' background='black' />
-      <ProductsList games={gameSimulacao} title='Simulação' background='gray' />
-    </>
-  )
+  if (actionGames && sportnGames && simulationGames && fightGames && rpgGames) {
+    return (
+      <>
+        <ProductsList games={actionGames} title='Ação' background='gray' />
+        <ProductsList games={sportnGames} title='Esportes' background='black' />
+        <ProductsList games={fightGames} title='Luta' background='gray' />
+        <ProductsList games={rpgGames} title='rpg' background='black' />
+        <ProductsList games={simulationGames} title='Simulação' background='gray' />
+      </>
+    )
+  }
+  return <h4>Carregando...</h4>
 }
 
 export default Categories
--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -2,44 +2,36 @@ import { useParams } from "react-router-dom"
 import Hero from "../../components/Hero"
 import Section from "../../components/Section"
 import Galley from "../../components/Gallery"
-import { useEffect, useState } from "react"
-import { Game } from "../Home"
+import { useGetGameQuery } from "../../services/api"
 
 const Product = () => {
-    const {id} = useParams()
+  const { id } = useParams()
+  const { data: game } = useGetGameQuery(id!)
 
-    const [game, setGame] = useState<Game>()
+  if (!game) {
+    return <h3>Carregando...</h3>
+  }
 
-    useEffect(() => {
-      fetch(`https://fake-api-tau.vercel.app/api/eplay/jogos/${id}`)
-        .then((res) => res.json())
-        .then((res) => setGame(res))
-    }, [id])
-
-    if(!game) {
-      return <h3>Carregando...</h3>
-    }
-
-    return (
-      <>
-        <Hero game={game}/>
-        <Section title="Sobre o jogo" background="black">
-          <p>{game.description}</p>
-        </Section>
-        <Section title="Mais detalhes" background="gray">
-          <p>
-            <b>Plataforma: </b>{game.details.system}<br/>
-            <b>Desenvolvedor: </b>{game.details.developer}<br/>
-            <b>Editora: </b>{game.details.publisher}<br/>
-            <b>Idiomas: </b>{game.details.languages.join(', ')}<br/>
-          </p>
-        </Section>
-        <Galley
-          name={game.name}
-          defaultCover={game.media.cover}
-          items={game.media.gallery}/>
-      </>
-    )
+  return (
+    <>
+      <Hero game={game} />
+      <Section title="Sobre o jogo" background="black">
+        <p>{game.description}</p>
+      </Section>
+      <Section title="Mais detalhes" background="gray">
+        <p>
+          <b>Plataforma: </b>{game.details.system}<br />
+          <b>Desenvolvedor: </b>{game.details.developer}<br />
+          <b>Editora: </b>{game.details.publisher}<br />
+          <b>Idiomas: </b>{game.details.languages.join(', ')}<br />
+        </p>
+      </Section>
+      <Galley
+        name={game.name}
+        defaultCover={game.media.cover}
+        items={game.media.gallery} />
+    </>
+  )
 }
 
 export default Product
